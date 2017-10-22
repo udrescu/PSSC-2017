037e82e29df2470f4e3b5e54bd02f6c06dce9885
Rares <ppmldvn96@yahoo.com>
2017-10-22 11:21:47 +0300

    Tema1-Curs

---
+++ Pop-Moldovan Rares/Laborator/Lucrarea2/Tema2/SRP/DataAccessLayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Examples.SRP
+{
+    public class DataAccessLayer
+    {
+        public void StoreUserInfo(string encPassword, string encUserName)
+        {
+            using (SqlConnection sql = new SqlConnection("connection-string"))
+            {
+                sql.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "INSERT INTO ... ";
+                command.Parameters.Add(encPassword);
+                command.Parameters.Add(encUserName);
+                command.BeginExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ Pop-Moldovan Rares/Laborator/Lucrarea2/Tema2/SRP/SingleResponsabilityPrinciple.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+namespace Examples.SRP
+{
+    public class SingleResponsabilityPrinciple
+    {
+        private EmailManager _emailManager;
+        private CryptoProvider _cryptoProvider;
+        private DataAccessLayer _dal;
+
+        public SingleResponsabilityPrinciple(EmailManager emailManager, CryptoProvider cryptoProvider, DataAccessLayer dal)
+        {
+            _emailManager = emailManager;
+            _cryptoProvider = cryptoProvider;
+            _dal = dal;
+        }
+
+        public void RegisterUserIntoApplication(string password, string userName, string email)
+        {
+            if (_emailManager.ValidateEmail(email))
+            {
+                _dal.StoreUserInfo(_cryptoProvider.EncryptWithAes(password), _cryptoProvider.EncryptWithAes(userName));
+                _emailManager.SendConfirmationEmailToUser(email);
+            }
+        }
+    }
+}
