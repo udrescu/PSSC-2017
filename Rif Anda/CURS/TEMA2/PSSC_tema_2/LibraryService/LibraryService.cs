91375159744e65992c3e4d0ea3e7239b17b5c579
Anda-R <aeaegd37@gmail.com>
2017-11-08 21:43:56 +0200

    tema2 - curs

---
+++ Rif Anda/CURS/TEMA2/PSSC_tema_2/LibraryService/LibraryService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryService
+{
+    public class LibraryService
+    {
+        public void insertLibrary(LibraryModel.Library l)
+        {
+            LibraryRepository.Library repo = new LibraryRepository.Library();
+            repo.AddLibrary(l);
+        }
+
+        public List<string> allLibraries()
+        {
+            LibraryRepository.Library repo = new LibraryRepository.Library();
+            return repo.listLibraries();
+        }
+
+        public void addBook(LibraryModel.Book b)
+        {
+            LibraryRepository.Library repo = new LibraryRepository.Library();
+            repo.AddBook(b);
+        }
+
+        public List<string> allBooks(string library)
+        {
+            LibraryRepository.Library repo = new LibraryRepository.Library();
+            return repo.listBooks(library);
+        }
+    }
+}
