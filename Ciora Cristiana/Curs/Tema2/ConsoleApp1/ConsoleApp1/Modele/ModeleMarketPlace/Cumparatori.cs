65011987d5437d407f3bc253b04328c0d1e54c77
cioracristiana <32551923+cioracristiana@users.noreply.github.com>
2017-11-09 22:55:39 +0200

    Tema 2

---
+++ Ciora Cristiana/Curs/Tema2/ConsoleApp1/ConsoleApp1/Modele/ModeleMarketPlace/Cumparatori.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Modele.ModeleMarketPlace
+{
+    class Cumparatori
+    {
+        private List<Cumparator> cumparatori = new List<Cumparator>();
+
+
+        public void AdaugaCumparator(Cumparator cumparator)
+        {
+            cumparatori.Add(cumparator);
+        }
+
+        public List<Cumparator> GetCumparatori
+        {
+            get { return cumparatori; }
+        }
+    }
+}
---
+++ Ciora Cristiana/Curs/Tema2/ConsoleApp1/ConsoleApp1/Modele/ModeleMarketPlace/MarketPlace.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.ModelGeneric;
+
+namespace ConsoleApp1.Modele.ModeleMarketPlace
+{
+    class MarketPlace
+    {
+
+        private string nume;
+        private List<Vanzator> vanzatori { get; set; }
+        private List<Cumparator> cumparatori { get; set; }
+
+
+
+        public MarketPlace(string nume, List<Vanzator> vanzatori, List<Cumparator> cumparatori)
+        {
+            this.nume = nume;
+            this.vanzatori = vanzatori;
+            this.cumparatori = cumparatori;
+        }
+
+        public MarketPlace()
+        {
+        }
+
+        void AdaugaCumparator(Cumparator cumparator, Cumparatori cumparatori)
+        {
+            cumparatori.AdaugaCumparator(cumparator);
+        }
+
+        void AdaugaVanzator(Vanzator vanzator, Vanzatori vanzatori)
+        {
+            vanzatori.AdaugaVanzator(vanzator);
+        }
+
+        public List<Cumparator> GetCumparatori
+        {
+            get { return cumparatori; }
+        }
+
+        public List<Vanzator> GetVanzatori
+        {
+            get { return vanzatori; }
+        }
+    }
+}
