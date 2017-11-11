4265cd3220485847963ab85dbcb128d34bcd448e
Diana Popoviciu <diana.popoviciu1@gmail.com>
2017-11-11 10:09:59 +0200

    Add Depozitare Colet operation

---
+++ DIANA_POPOVICIU/CURS/TEMA 2/WindowsFormsApp1/WindowsFormsApp1/Services/DepozitareColet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.Models.Entities;
+using WindowsFormsApp1.Repository;
+
+namespace WindowsFormsApp1.Services
+{
+    class DepozitareColet
+    {
+        public static void eliberare_colet(string numar_colet)
+        {
+            foreach (Colet colet in StoreColete.listColete)
+            {
+                if (colet.Numar_inregistrare.CompareTo(numar_colet) == 0)
+                {
+                    colet.Depozitat = true;
+                }
+            }
+        }
+    }
+}
